64fb9ea351b4f0a5c7f0ad9b4c527b08d704b49d
Ingmar uit de Bos <ingmar.uitdebos@vyoma.de>
2021-09-17 22:26:06 +0200

    Blender: More control over directions, and allow skipping frames

--- blender-d2tools/d2tools.py
+++ blender-d2tools/d2tools.py
@@ -124,13 +124,20 @@ class VIEW3D_PT_D2ToolsRenderProperties(bpy.types.Panel):
             row = layout.row()
             row.label(text = "Directions to render")
             row = layout.row()
-            row.prop(context.scene, "d2tools_directions", expand = True)
+            row.prop(context.scene, "d2tools_startDirection")
             row = layout.row()
-            row.prop(context.scene, "d2tools_skipDirections")
+            row.prop(context.scene, "d2tools_endDirection")
             row = layout.row()
             row.prop(context.scene, "frame_start", text = "Frame start")
             row = layout.row()
             row.prop(context.scene, "frame_end", text = "Frame end")
+            row = layout.row()
+            row.prop(context.scene, "d2tools_frameSkip")
+        
+        
+        if (context.scene.d2tools_types == "D2TILE"):
+            row = layout.row()
+            row.label(text = "WIP. Render manually.")
             
             
         if (context.scene.d2tools_types == "D2ITEM"):
@@ -155,6 +162,10 @@ class VIEW3D_PT_D2ToolsRender(bpy.types.Panel):
         if (context.scene.d2tools_types == "D2ISO"):
             row = layout.row()
             row.operator("d2tools.ops_render")
+            
+        if (context.scene.d2tools_types == "D2TILE"):
+            row = layout.row()
+            row.operator("render.render")
             
         if (context.scene.d2tools_types == "D2ITEM"):
             row = layout.row()
@@ -183,19 +194,50 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         cam = bpy.data.objects.new('CAMERA', cam_data)
         bpy.context.collection.objects.link(cam)
         bpy.context.scene.camera = cam
-        cam.location = (0, -30, 20)
+        cam.location = (0, -10, 8.5)
         cam.rotation_euler[0] = math.radians(60)
         cam.rotation_euler[1] = math.radians(0)
         cam.rotation_euler[2] = math.radians(0)
         cam.data.type = 'ORTHO'
         cam.data.ortho_scale = 7
         
+        # Generate light source
+        light_data = bpy.data.lights.new('light', type='SUN')
+        light = bpy.data.objects.new('LIGHT', light_data)
+        bpy.context.collection.objects.link(light)
+        light.location = (8.05, -11.788, 24)
+        light.rotation_euler[0] = math.radians(0)
+        light.rotation_euler[1] = math.radians(30.7)
+        light.rotation_euler[2] = math.radians(-55.7)
+        
+        # Parent camera and light to rotatebox, then rotate box to direction 0
+        cam.parent = rotatebox
+        light.parent = rotatebox
+        rotatebox.rotation_euler[0] = math.radians(0)
+        rotatebox.rotation_euler[1] = math.radians(0)
+        rotatebox.rotation_euler[2] = math.radians(45)
+        
+        # Don't allow user to meddle with settings by default
+        cam.hide_select = True
+        light.hide_select = True
+        rotatebox.hide_select = True
+        
+        return True
+    
+    def generate_objects_tile(self, context):
+        
+        # Generate rotatebox
+        rotatebox = bpy.data.objects.new('ROTATEBOX', None)
+        bpy.context.collection.objects.link(rotatebox)
+        rotatebox.empty_display_size = 0.5
+        rotatebox.empty_display_type = 'PLAIN_AXES'
+        
         # Generate floor tile camera
         fs_cam_data = bpy.data.cameras.new('camera')
         fs_cam = bpy.data.objects.new('FS_CAMERA', fs_cam_data)
         bpy.context.collection.objects.link(fs_cam)
         bpy.context.scene.camera = fs_cam
-        fs_cam.location = (0, -34.65, 20)
+        fs_cam.location = (0, -34.66, 20)
         fs_cam.rotation_euler[0] = math.radians(60)
         fs_cam.rotation_euler[1] = math.radians(0)
         fs_cam.rotation_euler[2] = math.radians(0)
@@ -212,7 +254,6 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         light.rotation_euler[2] = math.radians(-55.7)
         
         # Parent camera and light to rotatebox, then rotate box to direction 0
-        cam.parent = rotatebox
         fs_cam.parent = rotatebox
         light.parent = rotatebox
         rotatebox.rotation_euler[0] = math.radians(0)
@@ -220,12 +261,12 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         rotatebox.rotation_euler[2] = math.radians(45)
         
         # Don't allow user to meddle with settings by default
-        cam.hide_select = True
         fs_cam.hide_select = True
         light.hide_select = True
         rotatebox.hide_select = True
         
         return True
+    
     
     def generate_objects_item(self, context):
         
@@ -254,14 +295,6 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         light.location = (0.1, -0.7, -0.3)
         light.energy = 50
         light.shadow_soft_size = 0.1
-        
-        # sun backup
-        # light.location = (2.3, -2.78, 6.8)
-        # light.rotation_euler[0] = math.radians(0)
-        # light.rotation_euler[1] = math.radians(60)
-        # light.rotation_euler[2] = math.radians(-75)
-        # light.energy = 2
-        # light.angle = math.radians(10)
         
         # Parent camera and light to rotatebox
         cam.parent = rotatebox
@@ -306,9 +339,20 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         new_bodypart('Human_Leg_L', (0.15, 0.15, 0.8), (0.15, 0, 0.4))
         new_bodypart('Human_Leg_R', (0.15, 0.15, 0.8), (-0.15, 0, 0.4))
         
+        return True
+    
+    def generate_examples_tile(self, context):
+        
+        # Create root object for approximate scale example
+        scales = bpy.data.objects.new('_Approximate_Scale_', None)
+        bpy.context.collection.objects.link(scales)
+        scales.empty_display_size = 0.5
+        scales.empty_display_type = 'ARROWS'
+        scales.hide_render = True
+        
         # Floor tile
         bpy.ops.mesh.primitive_plane_add(
-            size = 2,
+            size = 1.98,
             enter_editmode = False,
             align = 'WORLD',
         )
@@ -371,7 +415,7 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         
         # Disable anti-alias
         bpy.context.scene.cycles.pixel_filter_type = 'GAUSSIAN'
-        bpy.context.scene.cycles.filter_width = 0.02
+        bpy.context.scene.cycles.filter_width = 0.01
         
         # Black background
         bpy.context.scene.render.film_transparent = True
@@ -387,6 +431,18 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         bpy.context.scene.render.resolution_x = 256
         bpy.context.scene.render.resolution_y = 256
         bpy.context.scene.frame_end = 8
+        
+        return True
+    
+    
+    def scene_setup_tile(self, context):
+        
+        self.scene_setup_common(context)
+        
+        # Output settings
+        bpy.context.scene.render.resolution_x = 160
+        bpy.context.scene.render.resolution_y = 80
+        bpy.context.scene.frame_end = 1
         
         return True
     
@@ -408,18 +464,24 @@ class D2TOOLS_OT_generate(bpy.types.Operator):
         if (context.scene.d2tools_generateRotatebox):
             if (context.scene.d2tools_types == "D2ISO"):
                 self.generate_objects_iso(context)
+            elif (context.scene.d2tools_types == "D2TILE"):
+                self.generate_objects_tile(context)
             elif (context.scene.d2tools_types == "D2ITEM"):
                 self.generate_objects_item(context)
             
         if (context.scene.d2tools_generateExamples):
             if (context.scene.d2tools_types == "D2ISO"):
                 self.generate_examples_iso(context)
+            elif (context.scene.d2tools_types == "D2TILE"):
+                self.generate_examples_tile(context)
             elif (context.scene.d2tools_types == "D2ITEM"):
                 self.generate_examples_item(context)
         
         if (context.scene.d2tools_generateSceneSetup):
             if (context.scene.d2tools_types == "D2ISO"):
                 self.scene_setup_iso(context)
+            elif (context.scene.d2tools_types == "D2TILE"):
+                self.scene_setup_tile(context)
             elif (context.scene.d2tools_types == "D2ITEM"):
                 self.scene_setup_item(context)
         
@@ -437,16 +499,18 @@ class D2TOOLS_OT_render(bpy.types.Operator):
     def execute(self, context):
         outputDir = context.scene.d2tools_outputDir
         fileName = context.scene.d2tools_fileName
-        numDirections = int(context.scene.d2tools_directions, base=10)
-        skipDirections = context.scene.d2tools_skipDirections
-        
-        if (skipDirections >= numDirections):
-            skipDirections = numDirections - 1
+        startDirection = context.scene.d2tools_startDirection
+        endDirection = context.scene.d2tools_endDirection
+
+        if (endDirection < startDirection):
+            context.scene.d2tools_endDirection = startDirection
+            endDirection = startDirection
 
         # This object should contain your camera and your point light
         rotatebox = bpy.data.objects["ROTATEBOX"]
         startFrame = bpy.context.scene.frame_start
         endFrame = bpy.context.scene.frame_end
+        frameSkip = context.scene.d2tools_frameSkip + 1
         
         # If transparent, set ambient light. Otherwise set opaque background
         if (bpy.context.scene.render.film_transparent):
@@ -455,25 +519,22 @@ class D2TOOLS_OT_render(bpy.types.Operator):
             bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = d2_background
         
         # Render the frames for each direction
-        for i in range(skipDirections, numDirections):
+        for d in range(startDirection, endDirection + 1):
             # Rotate the ROTATEBOX
-            rotatebox.rotation_euler[0] = math.radians( d2_directions[i][0] )
-            rotatebox.rotation_euler[1] = math.radians( d2_directions[i][1] )
-            rotatebox.rotation_euler[2] = math.radians( d2_directions[i][2] )
+            rotatebox.rotation_euler[0] = math.radians( d2_directions[d][0] )
+            rotatebox.rotation_euler[1] = math.radians( d2_directions[d][1] )
+            rotatebox.rotation_euler[2] = math.radians( d2_directions[d][2] )
 
             # Render animation
-            for f in range(startFrame, endFrame + 1):
-                bpy.context.scene.frame_set( f ) # Set frame
+            endWithSkips = math.floor((endFrame-startFrame) / frameSkip) + startFrame + 1
+            for f in range(startFrame, endWithSkips):
+                target_frame = f + (f-startFrame) * (frameSkip-1)
+                bpy.context.scene.frame_set( target_frame ) # Set frame
                 
                 # Output definitions
-                dirNum = str( f ).zfill(2) # Zero-padds direction number (5 -> 05)
-                frameNum = str( f ).zfill(4) # Zero-padds frame number (5 -> 0005)
-                frameName = "{prefix}_{dir}_{f}{ext}".format(
-                    prefix = fileName,
-                    dir = dirNum,
-                    f = frameNum,
-                    ext = bpy.context.scene.render.file_extension,
-                )
+                dirNum = str( d ).zfill(2) # Zero-padds direction number (5 -> 05)
+                frameNum = str( target_frame ).zfill(4) # Zero-padds frame number (5 -> 0005)
+                frameName = f"{fileName}_{dirNum}_{frameNum}{bpy.context.scene.render.file_extension}"
                 bpy.context.scene.render.filepath = join( outputDir, frameName )
 
                 # Render frame
@@ -528,7 +589,8 @@ d2tools_types = bpy.props.EnumProperty(
     name = 'Graphic type',
     description = 'The type of content to render',
     items = [
-        ('D2ISO', 'World', 'Anything in the game world; characters, missiles, objects, environments'),
+        ('D2ISO', 'Entity', 'Anything 3D in the game world; characters, missiles, objects'),
+        ('D2TILE', 'Tile', 'Environment pieces, floor tiles, walls etc.'),
         ('D2ITEM', 'Item', 'Items are rendered straight on'),
     ],
     default = 'D2ISO',
@@ -564,25 +626,28 @@ d2tools_fileName = bpy.props.StringProperty(
     description = "Output will be <file_name>_<direction>_<frame_number>.\n\nRecommendation is the name the animation will have, using D2 naming structure: <token><bodypart><armor><animation><hitclass>. As an example, Corrupt Rogue uses CRTRLITNUHTH",
 )
 
-d2tools_directions = bpy.props.EnumProperty(
-    name = 'Directions',
-    description = 'Number of directions to render',
-    items = [
-        ('1', '1', 'Renders only facing bottom left corner.'),
-        ('4', '4', 'Renders facing all 4 corners.'),
-        ('8', '8', '(Monsters) Renders facing all 4 corners and straights.'),
-        ('16', '16', '(Player Characters) Renders all including in-between steps.'),
-        ('32', '32', '(Missiles) Renders all possible directions D2 can handle.'),
-    ],
-    default = '8',
-)
-
-d2tools_skipDirections = bpy.props.IntProperty(
-    name = 'Skip Directions',
-    description = 'Number of directions to skip. Only useful if Blender crashes while rendering.',
+d2tools_frameSkip = bpy.props.IntProperty(
+    name = 'Frame skip',
+    description = 'Skip every X frames. Frame skip of 1 renders frames 0, 2, 4, 6 etc.',
     default = 0,
     min = 0,
-    max = 32,
+    max = 50,
+)
+
+d2tools_startDirection = bpy.props.IntProperty(
+    name = 'Start direction',
+    description = 'Number of starting direction. 0 is facing bottom-left.',
+    default = 0,
+    min = 0,
+    max = 31,
+)
+
+d2tools_endDirection = bpy.props.IntProperty(
+    name = 'End direction',
+    description = 'Number of end direction. Only useful if Blender crashes while rendering.',
+    default = 0,
+    min = 0,
+    max = 31,
 )
 
 registerClasses = [
@@ -602,8 +667,9 @@ def register():
     bpy.types.Scene.d2tools_generateSceneSetup = d2tools_generateSceneSetup
     bpy.types.Scene.d2tools_outputDir = d2tools_outputDir
     bpy.types.Scene.d2tools_fileName = d2tools_fileName
-    bpy.types.Scene.d2tools_directions = d2tools_directions
-    bpy.types.Scene.d2tools_skipDirections = d2tools_skipDirections
+    bpy.types.Scene.d2tools_frameSkip = d2tools_frameSkip
+    bpy.types.Scene.d2tools_startDirection = d2tools_startDirection
+    bpy.types.Scene.d2tools_endDirection = d2tools_endDirection
     
     for c in registerClasses:
         bpy.utils.register_class(c)
@@ -615,8 +681,9 @@ def unregister():
     del bpy.types.Scene.d2tools_generateSceneSetup
     del bpy.types.Scene.d2tools_outputDir
     del bpy.types.Scene.d2tools_fileName
-    del bpy.types.Scene.d2tools_directions
-    del bpy.types.Scene.d2tools_skipDirections
+    del bpy.types.Scene.d2tools_frameSkip
+    del bpy.types.Scene.d2tools_startDirection
+    del bpy.types.Scene.d2tools_endDirection
     
     for c in registerClasses:
         bpy.utils.unregister_class(c)
